60458dd3b5b3f9ed1b1e13175babafc84fb87d23
LetsDanse <142117914+LetsDanse@users.noreply.github.com>
2026-05-01 01:23:08 -0700

    Update rubika_uploader.py

--- rubika_uploader.py
+++ rubika_uploader.py
@@ -27,7 +27,7 @@ async def upload_task(app, target_guid, file_path, file_name):
 
 async def main():
     # نام سشن که توسط گیت‌هاب بازیابی می‌شود
-    session_name = os.getenv('RUBIKA_SESSION_NAME')
+    session_name = "user_session"
     
     if not session_name:
         print("Error: RUBIKA_SESSION_NAME environment variable is not set!")
